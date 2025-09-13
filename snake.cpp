61641065737ef64babe323acf70039bdeb5ed93d
Muhammad Talha <talharaheem033@gmail.com>
2025-09-13 09:44:53 +0500

    Add files via upload

---
+++ snake.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <conio.h>   // For _kbhit() and _getch()
+#include <windows.h> // For Sleep()
+#include <cstdlib>   // For rand()
+using namespace std;
+
+const int width = 30;
+const int height = 20;
+bool gameOver;
+
+int x, y;            // Snake head
+int foodX, foodY;    // Food position
+int score;
+int tailX[100], tailY[100]; // Snake tail
+int nTail;
+
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    foodX = rand() % width;
+    foodY = rand() % height;
+    score = 0;
+    nTail = 0;
+}
+
+void Draw() {
+    system("cls"); // Clear console
+
+    // Top border
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) cout << "#"; // Left wall
+
+            if (i == y && j == x) cout << "O"; // Snake head
+            else if (i == foodY && j == foodX) cout << "F"; // Food
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o"; // Tail
+                        print = true;
+                    }
+                }
+                if (!print) cout << " ";
+            }
+
+            if (j == width - 1) cout << "#"; // Right wall
+        }
+        cout << endl;
+    }
+
+    // Bottom border
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    cout << "Score: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a': dir = LEFT; break;
+        case 'd': dir = RIGHT; break;
+        case 'w': dir = UP; break;
+        case 's': dir = DOWN; break;
+        case 'x': gameOver = true; break;
+        }
+    }
+}
+
+void Logic() {
+    // Save previous position of head
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    // Movement
+    switch (dir) {
+    case LEFT:  x--; break;
+    case RIGHT: x++; break;
+    case UP:    y--; break;
+    case DOWN:  y++; break;
+    }
+
+    // Collision with walls
+    if (x < 0 || x >= width || y < 0 || y >= height)
+        gameOver = true;
+
+    // Collision with tail
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+
+    // Eat food
+    if (x == foodX && y == foodY) {
+        score += 10;
+        foodX = rand() % width;
+        foodY = rand() % height;
+        nTail++;
+    }
+}
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(100); // Control game speed
+    }
+    cout << "Game Over! Final Score = " << score << endl;
+    return 0;
+}
+
